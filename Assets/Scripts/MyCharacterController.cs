ad3deaf13fcef00d3f0559ff018ddca0b9fd356d
Bucadyz <jo89hn@yahoo.com>
2018-11-19 19:59:26 -0600

    JamVember Submit 1

---
+++ Assets/Scripts/MyCharacterController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MyCharacterController : MonoBehaviour {
+
+    public float speed = 10.0f;
+    public Text countText;
+    public Text winText;
+    public float jump = 10.0f;
+
+    private Rigidbody rb;
+    private int count;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        rb = GetComponent<Rigidbody>();
+        count = 7;
+        SetCountText();
+        winText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal") * speed;
+        float moveVertical = Input.GetAxis("Vertical") * speed;
+
+        transform.Translate(moveHorizontal, 0, moveVertical);
+      
+    }
+   
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count - 1;
+            SetCountText();
+        }
+
+        if (other.gameObject.CompareTag("JumpPad"))
+        {
+            this.gameObject.GetComponent<Rigidbody>
+                ().AddForce(Vector3.up * jump);
+        }      
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Objects Remaining: " + count.ToString();
+        if (count == 0)
+        {
+            winText.text = "You win!";
+        }
+    }
+}
+
---
+++ Assets/Scripts/TeleportPad.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportPad : MonoBehaviour {
+
+    public float teleportx = 0.0f;
+    public float teleporty = 1.0f;
+    public float teleportz = 0.0f;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.transform.position = new Vector3(teleportx, teleporty, teleportz);
+        }      
+    }
+}
